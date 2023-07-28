ef7744d92927cb8a4c6b7aeffcf141c046ed58c6
Pickeldj <j.strydom93@outlook.com>
2023-07-28 16:33:44 +0200

    foundry project foundry-FundMe-f23 v0.1.3  Changes to be committed: 	modified:   .gitmodules 	new file:   Makefile 	modified:   README.md 	modified:   foundry.toml 	new file:   lib/foundry-devops 	modified:   script/DeployFundMe.s.sol 	modified:   script/HelperConfig.s.sol 	new file:   script/Interactions.s.sol 	modified:   src/FundMe.sol 	deleted:    test/FundMeTest.t.sol 	new file:   test/integration/InteractionsTest.t.sol 	new file:   test/uint/FundMeTest.t.sol

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -1,59 +1,48 @@
-//SPDX-License-Identifier: MIT
-pragma solidity ^0.8.18;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
 
-import {Script} from "../forge-std/Script.sol";
-import {FundMe} from "../src/FundMe.sol";
 import {MockV3Aggregator} from "../test/mocks/MockV3Aggregator.sol";
-
-// This is the contract that allows us to set the price feed address for the network we are on.
-// We have a different price feed address for each network.
-// We need to know the price of ETH in order to calculate the price of the Anvil product in USD.
-// We use the price feed address to get the price of ETH.
-// This contract is deployed on each network.
-// We use the network ID to determine which price feed to use.
-// The price feed is updated by Chainlink every 1 hour.
+import {Script} from "forge-std/Script.sol";
 
 contract HelperConfig is Script {
-    struct NetworkConfig {
-        address priceFeed;
-    }
-
     NetworkConfig public activeNetworkConfig;
 
     uint8 public constant DECIMALS = 8;
     int256 public constant INITIAL_PRICE = 2000e8;
 
+    struct NetworkConfig {
+        address priceFeed;
+    }
+
+    event HelperConfig__CreatedMockPriceFeed(address priceFeed);
+
     constructor() {
         if (block.chainid == 11155111) {
             activeNetworkConfig = getSepoliaEthConfig();
-        } else if (block.chainid == 1) {
-            activeNetworkConfig = getMainnetEthConfig();
         } else {
-            activeNetworkConfig = getAnvilEthConfig();
+            activeNetworkConfig = getOrCreateAnvilEthConfig();
         }
     }
 
-    function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
-        NetworkConfig memory sepoliaConfig = NetworkConfig({priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306});
-        return sepoliaConfig;
+    function getSepoliaEthConfig() public pure returns (NetworkConfig memory sepoliaNetworkConfig) {
+        sepoliaNetworkConfig = NetworkConfig({
+            priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306 // ETH / USD
+        });
     }
 
-    function getMainnetEthConfig() public pure returns (NetworkConfig memory) {
-        NetworkConfig memory mainnetConfig = NetworkConfig({priceFeed: 0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419});
-        return mainnetConfig;
-    }
-
-    function getAnvilEthConfig() public returns (NetworkConfig memory) {
+    function getOrCreateAnvilEthConfig() public returns (NetworkConfig memory anvilNetworkConfig) {
+        // Check to see if we set an active network config
         if (activeNetworkConfig.priceFeed != address(0)) {
             return activeNetworkConfig;
         }
         vm.startBroadcast();
         MockV3Aggregator mockPriceFeed = new MockV3Aggregator(
-                DECIMALS, 
-                INITIAL_PRICE);
+            DECIMALS,
+            INITIAL_PRICE
+        );
         vm.stopBroadcast();
+        emit HelperConfig__CreatedMockPriceFeed(address(mockPriceFeed));
 
-        NetworkConfig memory anvilConfig = NetworkConfig({priceFeed: address(mockPriceFeed)});
-        return anvilConfig;
+        anvilNetworkConfig = NetworkConfig({priceFeed: address(mockPriceFeed)});
     }
 }
