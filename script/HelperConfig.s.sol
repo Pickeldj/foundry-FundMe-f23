1d1cad196c5cc191185ff8ca6b9ec5dccae348ca
Pickeldj <j.strydom93@outlook.com>
2023-07-28 14:59:40 +0200

    foundry project foundry-FundMe-f23 v 0.1.2 	new file:   .gas-snapshot 	modified:   script/HelperConfig.s.sol 	modified:   src/FundMe.sol 	modified:   test/FundMeTest.t.sol 	modified:   test/mocks/MockV3Aggregator.sol

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -5,7 +5,6 @@ import {Script} from "../forge-std/Script.sol";
 import {FundMe} from "../src/FundMe.sol";
 import {MockV3Aggregator} from "../test/mocks/MockV3Aggregator.sol";
 
-
 // This is the contract that allows us to set the price feed address for the network we are on.
 // We have a different price feed address for each network.
 // We need to know the price of ETH in order to calculate the price of the Anvil product in USD.
@@ -15,52 +14,46 @@ import {MockV3Aggregator} from "../test/mocks/MockV3Aggregator.sol";
 // The price feed is updated by Chainlink every 1 hour.
 
 contract HelperConfig is Script {
-        struct NetworkConfig{
-            address priceFeed;
-        }
+    struct NetworkConfig {
+        address priceFeed;
+    }
 
-        NetworkConfig public activeNetworkConfig;
+    NetworkConfig public activeNetworkConfig;
 
-        uint8 public constant DECIMALS = 8;
-        int256 public constant INITIAL_PRICE = 2000e8;
+    uint8 public constant DECIMALS = 8;
+    int256 public constant INITIAL_PRICE = 2000e8;
 
-        constructor(){
-            if (block.chainid == 11155111){
-                activeNetworkConfig = getSepoliaEthConfig();
-            } else if (block.chainid == 1){
-                activeNetworkConfig = getMainnetEthConfig();
-            } else {
-                activeNetworkConfig = getAnvilEthConfig();
-            }
-        }
-
-        function getSepoliaEthConfig() public pure returns(NetworkConfig memory){
-            NetworkConfig memory sepoliaConfig = NetworkConfig({
-                priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306
-            });
-            return sepoliaConfig;  
-        }
-
-        function getMainnetEthConfig() public pure returns(NetworkConfig memory){
-            NetworkConfig memory mainnetConfig = NetworkConfig({
-                priceFeed: 0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419
-            });
-            return mainnetConfig;  
-        }
-
-        function getAnvilEthConfig() public returns(NetworkConfig memory){
-            if (activeNetworkConfig.priceFeed != address(0)){
-                return activeNetworkConfig;
-            }
-              vm.startBroadcast();
-                MockV3Aggregator mockPriceFeed = new MockV3Aggregator(
-                DECIMALS, 
-                INITIAL_PRICE);
-              vm.stopBroadcast();
-
-                NetworkConfig memory anvilConfig = NetworkConfig({
-                    priceFeed: address(mockPriceFeed)
-                });
-                return anvilConfig;
+    constructor() {
+        if (block.chainid == 11155111) {
+            activeNetworkConfig = getSepoliaEthConfig();
+        } else if (block.chainid == 1) {
+            activeNetworkConfig = getMainnetEthConfig();
+        } else {
+            activeNetworkConfig = getAnvilEthConfig();
         }
     }
+
+    function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
+        NetworkConfig memory sepoliaConfig = NetworkConfig({priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306});
+        return sepoliaConfig;
+    }
+
+    function getMainnetEthConfig() public pure returns (NetworkConfig memory) {
+        NetworkConfig memory mainnetConfig = NetworkConfig({priceFeed: 0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419});
+        return mainnetConfig;
+    }
+
+    function getAnvilEthConfig() public returns (NetworkConfig memory) {
+        if (activeNetworkConfig.priceFeed != address(0)) {
+            return activeNetworkConfig;
+        }
+        vm.startBroadcast();
+        MockV3Aggregator mockPriceFeed = new MockV3Aggregator(
+                DECIMALS, 
+                INITIAL_PRICE);
+        vm.stopBroadcast();
+
+        NetworkConfig memory anvilConfig = NetworkConfig({priceFeed: address(mockPriceFeed)});
+        return anvilConfig;
+    }
+}
--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -12,9 +12,15 @@ import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 contract FundMeTest is Test {
     FundMe fundMe;
 
+    address User = makeAddr("User");
+    uint256 constant SEND_VALUE = 0.1 ether;
+    uint256 constant Starting_Balance = 10 ether;
+    uint256 constant GAS_PRICE = 1 gwei;
+
     function setUp() external {
         DeployFundMe deployFundMe = new DeployFundMe();
         fundMe = deployFundMe.run();
+        vm.deal(User, Starting_Balance);
     }
 
     function testMinimumDollaIsFive() public {
@@ -22,11 +28,96 @@ contract FundMeTest is Test {
     }
 
     function testOwnerIsMsgSender() public {
-        assertEq(fundMe.i_owner(), msg.sender);
+        assertEq(fundMe.getOwner(), msg.sender);
     }
 
     function testPriceFeedVersionIsAccurate() public {
         uint256 version = fundMe.getVersion();
         assertEq(version, 4);
+    }
+
+    function testFundFailsWithoutEnoughETH() public {
+        vm.expectRevert();
+        fundMe.fund();
+    }
+
+    function testFundUpdatesFundedDataStructure() public {
+        vm.prank(User);
+        fundMe.fund{value: SEND_VALUE}();
+
+        uint256 amountFunded = fundMe.getAddressToAmountFunded(User);
+        assertEq(amountFunded, SEND_VALUE);
+    }
+
+    function testAddsFunderToArrayOfFunders() public {
+        vm.prank(User);
+        fundMe.fund{value: SEND_VALUE}();
+
+        address funder = fundMe.getFunder(0);
+        assertEq(funder, User);
+    }
+
+    modifier funded() {
+        vm.prank(User);
+        fundMe.fund{value: SEND_VALUE}();
+        _;
+    }
+
+    function testOnlyOwnerCanWithdraw() public funded {
+        vm.expectRevert();
+        vm.prank(User);
+        fundMe.withdraw();
+    }
+
+    function testWithDrawWithSingleFunder() public funded {
+        // Arrange
+        uint256 startingOwnerBalance = fundMe.getOwner().balance;
+        uint256 startingFundMeBalance = address(fundMe).balance;
+        // Act
+        vm.prank(fundMe.getOwner());
+        fundMe.withdraw();
+        // Assert
+        uint256 endingOwnerBalance = fundMe.getOwner().balance;
+        uint256 endingFundMeBalance = address(fundMe).balance;
+        assertEq(endingFundMeBalance, 0);
+        assertEq(startingFundMeBalance + startingOwnerBalance, endingOwnerBalance);
+    }
+
+    function testWithdrawFromMultipleFunders() public funded {
+        uint160 numberOfFunders = 10;
+        uint160 startingFunderIndex = 1;
+        for (uint160 i = startingFunderIndex; i < numberOfFunders; i++) {
+            hoax(address(i), SEND_VALUE);
+            fundMe.fund{value: SEND_VALUE}();
+        }
+
+        uint256 startingOwnerBalance = fundMe.getOwner().balance;
+        uint256 startingFundMeBalance = address(fundMe).balance;
+
+        vm.startPrank(fundMe.getOwner());
+        fundMe.withdraw();
+        vm.stopPrank();
+
+        assert(address(fundMe).balance == 0);
+        assert(startingFundMeBalance + startingOwnerBalance == fundMe.getOwner().balance);
+    }
+
+        function testWithdrawFromMultipleFundersCheaper() public funded {
+        uint160 numberOfFunders = 10;
+        uint160 startingFunderIndex = 1;
+        for (uint160 i = startingFunderIndex; i < numberOfFunders; i++) {
+            hoax(address(i), SEND_VALUE);
+            fundMe.fund{value: SEND_VALUE}();
+        }
+
+        uint256 startingOwnerBalance = fundMe.getOwner().balance;
+        uint256 startingFundMeBalance = address(fundMe).balance;
+
+        vm.startPrank(fundMe.getOwner());
+        fundMe.cheaperWithdraw();
+        vm.stopPrank();
+
+        assert(address(fundMe).balance == 0);
+        assert(startingFundMeBalance + startingOwnerBalance == fundMe.getOwner().balance);
     }
 }
